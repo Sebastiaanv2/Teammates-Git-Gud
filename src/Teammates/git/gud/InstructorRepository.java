0c8e652c4eba998864019745b28c072954e5d196
Sebastiaan van Rijn <sebastiaan.94@live.nl>
2019-03-11 21:48:06 +0100

    fixed constructor for instructorrepo

--- src/Teammates/git/gud/InstructorRepository.java
+++ src/Teammates/git/gud/InstructorRepository.java
@@ -6,12 +6,17 @@ public class InstructorRepository {
     private Instructor c = new Instructor("001", "001", false, "Gert", "Gert@hu.nl", "a3em6jd", Role.Teacher);
     private Instructor d = new Instructor("002", "002", false, "Yeet", "Yeet@hu.nl", "df8gkdf", Role.Teacher);
     private Instructor e = new Instructor("003", null, false, "Meneer", "Meneer@hu.nl", "2lk43jo", Role.Teacher);
-    private ArrayList<Instructor> instructors = new ArrayList<>();
+    private ArrayList<Instructor> instructors;
 
-    public boolean updateCourseOfInstructor(String googleId, String newCourseId) {
+    public InstructorRepository() {
+        this.instructors = new ArrayList<>();
         instructors.add(c);
         instructors.add(d);
         instructors.add(e);
+    }
+
+
+    public boolean updateCourseOfInstructor(String googleId, String newCourseId) {
         for (Instructor i : instructors) {
             if (i.getGoogleId().equals(googleId)) {
                 i.setCourseId(newCourseId);
@@ -22,9 +27,6 @@ public class InstructorRepository {
     }
 
     public Instructor findInstructorById(String googleId) {
-        instructors.add(c);
-        instructors.add(d);
-        instructors.add(e);
         for (Instructor i : instructors) {
             if (i.getGoogleId().equals(googleId)) {
                 return i;
